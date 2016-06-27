099bf3c5ed79cb62274fd77a3355559ae4d25ec9
myyerrol <myyerrol@126.com>
2016-06-27 20:57:40 +0800

    Add the project4 of geekos

---
+++ experiment/geekos/project4/src/user/rec.c
@@ -0,0 +1,35 @@
+/*
+ * A user mode program which uses deep recursion
+ * to test stack growth.
+ */
+
+#include <conio.h>
+#include <string.h>
+
+void Recurse(int x)
+{
+    int stuff[512];
+
+    if (x == 0) return;
+
+    stuff[0] = x;
+    //Print("calling Recurse %d\n", x);
+    Recurse(x-1);
+	//Print("calling Bound %d\n", x);
+}
+
+int main(int argc, char **argv)
+{
+    /* change recurse to 5-10 to see stack faults without page outs */
+    int depth = 512;
+
+    if (argc > 1) {
+	depth = atoi(argv[1]);
+	Print("Depth is %d\n", depth);
+    }
+
+    Recurse(depth);
+	Print("done.\n");
+    return 0;
+}
+
